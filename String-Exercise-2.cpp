679ba666ba5e685d252bdb0096591b1b68e2a1c8
umisuhanarahman <33883955+umisuhanarahman@users.noreply.github.com>
2018-01-22 21:46:35 +0800

    task completed

--- String-Exercise-2.cpp
+++ String-Exercise-2.cpp
@@ -1,17 +1,24 @@
+//umi suhana bt abdul rahman
+//a17dw2041
+
 //append char1 with char2 with the right string function.
 //and display both char1 and char2
-#include <iostream>
 
-int main(){
+#include<iostream>
+#include<cstring>
+using namespace std;
 
-	char char1[11], char2[21];
+int main()
+{
+	char char1[5], char2[5];
 
-	strcpy(char1,"haillo");
-	strcpy(char2," there");
+	strcpy(char1,"hello");
+	strcpy(char2,"there");
 
 	cout<<char1<<endl;
 	cout<<char2<<endl;
 
 	return 0;
+
 }
 
