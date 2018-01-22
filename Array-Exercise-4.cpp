b3ff9590d90c813833d385d8a59a738e525b5952
umisuhanarahman <33883955+umisuhanarahman@users.noreply.github.com>
2018-01-22 10:21:05 +0800

    task complete

--- Array-Exercise-4.cpp
+++ Array-Exercise-4.cpp
@@ -1,33 +1,36 @@
+//umi suhana binti abdul rahman
+//a17dw2041
+
 //program to show a 3dimensional array
 //complete the unfinish and problamatic program below
 //thanks.
 
 #include<iostream>
-
+#include<conio.h>
 using namespace std;
 
 int main()
 {
 
-	int array[10][10],dim1,dim2,dim3,i,j,k;
-	cout<<"Enter the three dimensions size"<<end;
-	
+	int array[10][10][10],dim1,dim2,dim3,i,j,k;
+	cout<<"Enter the three dimensions size"<<endl;
+	cin>>dim1>>dim2>>dim3;
 	cout<<"Enter elements of array"<<endl;
 
-	for(/*idim1*/)
-		for(/*jdim2*/)
-			for(/*kdim3*/)
+	for(i=0;i<dim1;i++)
+		for(j=0;j<dim2;j++)
+			for(k=0;k<dim3;k++)
 			{
 				cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]=";
-				array[i][j][k];
+				cin>>array[i][j][k];
 			}
 
-	xour<<"Displaying elements of array"<<endl;
+	cout<<"Displaying elements of array"<<endl;
 
-	
-	for(/*idim1*/)
-		for(/*jdim2*/)
-			for(/*kdim3*/)
+	for(i=0;i<dim1;i++)
+		for(j=0;j<dim2;j++)
+			for(k=0;k<dim3;k++)
 				cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]="<<array[i][j][k]<<endl;
-				
+	
+	return 0;			
 }
